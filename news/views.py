1cacb016f4bee668baf7e16ea3edb728a004ba08
dev-yoga <spin_shoppe@icloud.com>
2022-09-21 22:41:02 -0700

    News works :)

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
 
 def news(request):
-    return render(request, 'news.html')
+    newss = News.objects.all()
+    return render(request, 'news.html', {'newss':newss})
