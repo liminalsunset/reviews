54927a8bf17637bb147899ec223758aadc8118e1
dev-yoga <spin_shoppe@icloud.com>
2022-09-25 19:23:36 -0700

    username already taken / check if username already exists

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.shortcuts import redirect
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -11,10 +12,13 @@ def signupaccount(request):
         return render(request, 'signupaccount.html', {'form':UserCreationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password= request.POST['password1'])
-            user.save()
-            login(request, user)
+            try:
+                user = User.objects.create_user(request.POST['username'], password= request.POST['password1'])
+                user.save()
+                login(request, user)
 # after someone signs up automatically redirect them to the home page:
-            return redirect('home')
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signupaccount.html', {'form':UserCreationForm, 'error':'Username already taken. Choose new username.'})
         else:
             return render(request, 'signupaccount.html', {'form':UserCreationForm, 'error':'Passwords do not match'})
