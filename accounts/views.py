f1c27c082e096a1d127f56fd84d8a91d31122921
dev-yoga <spin_shoppe@icloud.com>
2022-09-26 09:50:59 -0700

    user login works, invalid password/username combo error works, redirects work, logout, login, signup all happy

--- accounts/views.py
+++ accounts/views.py
@@ -36,7 +36,7 @@ def loginaccount(request):
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'loginaccount.html', {'form':AuthenticationForm(), 'error': 'username and password do not match'})
+            return render(request, 'loginaccount.html', {'form':AuthenticationForm(), 'error': 'Username and password do not match.'})
         else: 
             login(request,user)
             return redirect('home')
