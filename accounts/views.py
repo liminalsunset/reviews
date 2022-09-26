de0ad17a6837e4085a5e53f5f7a5d8e931cb897d
dev-yoga <spin_shoppe@icloud.com>
2022-09-26 09:41:55 -0700

    logout functionality, login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserCreateForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
+
 
 # Create your views here.
 
@@ -23,3 +25,18 @@ def signupaccount(request):
                 return render(request, 'signupaccount.html', {'form':UserCreateForm, 'error':'Username already taken. Choose new username.'})
         else:
             return render(request, 'signupaccount.html', {'form':UserCreateForm, 'error':'Passwords do not match'})
+
+def logoutaccount(request):        
+    logout(request)
+    return redirect('home')
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', {'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'loginaccount.html', {'form':AuthenticationForm(), 'error': 'username and password do not match'})
+        else: 
+            login(request,user)
+            return redirect('home')
