0498f49722ef7f43604434646443f3f07a103586
dev-yoga <spin_shoppe@icloud.com>
2022-09-28 20:19:01 -0700

    logged in user can add review

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [        
+urlpatterns = [
     path('<int:movie_id>', views.detail, name='detail'),
+    path('<int:movie_id>/create', views.createreview,
+         name='createreview'),
 ]
